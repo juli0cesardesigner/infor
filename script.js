0644fb0ad62d1ab77178289a47aa332bfa46e0dc
juli0cesardesigner <143847925+juli0cesardesigner@users.noreply.github.com>
2025-07-09 19:13:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Inicializa o Swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2.5, // Mostra 2.5 cards por vez
+  spaceBetween: 10,
+  pagination: { el: '.swiper-pagination' },
+  loop: false,
+});
