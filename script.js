2abb1b758b9c5acbf1d7bd6a4247df69657720ca
juli0cesardesigner <143847925+juli0cesardesigner@users.noreply.github.com>
2025-07-09 20:14:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
+  autoplay: {
+    delay: 3000,         // tempo entre os slides (em ms)
+    disableOnInteraction: true, // PAUSA ao tocar ou arrastar
+  },
 });
